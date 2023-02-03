75ab99aa20693865ed918440ae6ad3c075297364
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2023-02-03 18:17:52 -0300

    adjusting the scroll and the social networks

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function addClassActive(id) {
   }
 }
 
+const menuItems = document.querySelectorAll(".header-navigation a");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
+});
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute("href");
+  const toSection = document.querySelector(id).offsetTop;
+
+  window.scroll({
+    top: toSection - 140,
+    behavior: "smooth",
+  });
+}
